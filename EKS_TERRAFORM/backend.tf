cc8e702863901544249e6bbe9aa553167d687e3d
Sumit Ghoshal <sumitghoshal7666@gmail.com>
2026-08-15 10:40:40 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-2048-reyaz-4pm" # Replace with your actual S3 bucket name
+    bucket = "evsecops-2048-sumit143" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
